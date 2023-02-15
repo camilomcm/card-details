4efba8763311b18272657446b0521cebf0a30852
CAMILO <camilomc2796@gmail.com>
2023-02-15 05:02:12 -0500

    Agregando interactividad

--- script.js
+++ script.js
@@ -13,6 +13,29 @@ let inputCVC = document.querySelector('#cvc');
 let cvc = document.querySelector('#verification-code');
 
 let btnConfirm = document.querySelector('#confirm');
+let form = document.querySelector('form');
+
+btnConfirm.addEventListener('click', e => {
+    e.preventDefault();
+
+    let pBlank = document.createElement('p');
+    pBlank.textContent = 'Cant be blank';
+    pBlank.classList.add('error');
+    const errors = document.querySelectorAll('.error');
+
+    // if(inputName.textContent === '') { 
+    //     if(errors.length === 0) {
+    //         form.insertBefore(pBlank,document.querySelector('#card-label'));
+    //         // setTimeout(() => {
+    //         //     pBlank.remove();
+    //         // }, 3000);
+    //     }
+    // } else {
+    //     console.log('Niceeee')
+    // }
+
+    console.log(inputName.innerHTML)
+})
 
 let numberAlert = document.querySelector('#number-alert');
 
