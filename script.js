a4b680f279063c3904d45b0ad402d61cfcf5dd11
CAMILO <camilomc2796@gmail.com>
2023-02-17 05:02:48 -0500

    Validando numero de tarjeta

--- script.js
+++ script.js
@@ -13,31 +13,7 @@ let inputCVC = document.querySelector('#cvc');
 let cvc = document.querySelector('#verification-code');
 
 let btnConfirm = document.querySelector('#confirm');
-let form = document.querySelector('form');
-
-btnConfirm.addEventListener('click', e => {
-    e.preventDefault();
-    
-    var inputs = document.getElementsByTagName('input');
-    var warnings = document.getElementsByClassName('error');
-    while (warnings.length > 0) {
-        warnings[0].parentNode.removeChild(warnings[0]);
-    }
-
-    // Loop through each input element
-    for (var i = 0; i < inputs.length; i++) {
-        // Check if the input element is required and if it's empty
-        if (inputs[i].hasAttribute('required') && inputs[i].value === '' && inputs[i].id !== 'mm') {
-            var warning = document.createElement('p');
-            warning.innerHTML = 'Can\'t be blank';
-            warning.classList.add('error');
-            inputs[i].parentNode.insertBefore(warning, inputs[i].nextSibling);
-            setTimeout(() => {
-                warning.parentNode.removeChild(warning);
-            }, 3000);
-        }
-    }
-})  
+let form = document.querySelector('form');  
 
 inputName.addEventListener('input', (e) => {
     e.preventDefault();
@@ -52,17 +28,20 @@ inputNumber.addEventListener('input', (e) => {
     e.preventDefault();
     if(e.target.value === '') {
         cardNumber.textContent = '0000 0000 0000 0000';
-    } 
-    // else if(isNaN(e.target.value)) {
-    //     numberAlert.textContent = 'Wrong format, numbers only';
-    //     setTimeout(() => {
-    //         numberAlert.remove();
-    //     }, 3000);
-    // } 
-    else {
+    }  else {
         cardNumber.textContent = e.target.value;
+        validateCardNumber(e.target.value);
     }    
 });
+
+function validateCardNumber(number) {
+    let regEx = /^5[1-5][0-9]{14}$|^2(?:2(?:2[1-9]|[3-9][0-9])|[3-6][0-9][0-9]|7(?:[01][0-9]|20))[0-9]{12}$/;
+    if(number.match(regEx)) {
+        console.log('Niceeee')
+    } else {
+        console.log('Enter a valid number');
+    }
+}
 
 inputMonth.addEventListener('input', (e) => {
     e.preventDefault();
@@ -89,4 +68,31 @@ inputCVC.addEventListener('input', (e) => {
     } else {
         cvc.textContent = e.target.value;
     }    
+});
+
+btnConfirm.addEventListener('click', e => {
+    e.preventDefault();
+    
+    let inputs = document.getElementsByTagName('input');
+    let warning;
+
+    let warnings = document.querySelectorAll('.error');
+    for (let i = 0; i < warnings.length; i++) {
+        warnings[i].remove();
+        
+    }
+
+    // Loop through each input element
+    for (let i = 0; i < inputs.length; i++) {
+        // Check if the input element is required and if it's empty
+        if (inputs[i].hasAttribute('required') && inputs[i].value === '' && inputs[i].id !== 'mm') {
+            let warning = document.createElement('p');
+            warning.textContent = 'Can\'t be blank';
+            warning.classList.add('error');
+            inputs[i].parentNode.insertBefore(warning, inputs[i].nextSibling);
+            setTimeout(function() {
+                warning.remove();
+            }, 3000);
+        }
+    }
 });
