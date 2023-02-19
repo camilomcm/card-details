2b7ff0b6d5108a6fdb679b2c7e3130acaf554fda
CAMILO <camilomc2796@gmail.com>
2023-02-19 16:43:00 -0500

    Agregando mensaje de gracias

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ let cvc = document.querySelector('#verification-code');
 let btnConfirm = document.querySelector('#confirm');
 let form = document.querySelector('form');  
 
+let result = document.querySelector('#result');
+
 inputName.addEventListener('input', (e) => {
     e.preventDefault();
     if(e.target.value === '') {
@@ -36,10 +38,10 @@ inputNumber.addEventListener('input', (e) => {
 inputNumber.addEventListener('blur', e => {
     e.preventDefault();
     // let numberPattern = /^(\d{4})(\s)\1\2\1\2\1$/;
-    let numberPattern = /^\d+\s\d+\s\d+\s\d+$/;
+    let numberPattern = /^\d+\s\d+\s\d+\s\d{4}$/;
     if(!numberPattern.test(inputNumber.value)) {
         let warning = document.createElement('p');
-        warning.textContent = 'Wrong format';
+        warning.textContent = 'Wrong format, numbers only';
         warning.classList.add('error');
 
         const error = document.querySelectorAll('.error');
@@ -50,8 +52,6 @@ inputNumber.addEventListener('blur', e => {
         setTimeout(function() {
             warning.remove();
         }, 5000);
-    } else {
-        console.log('Niceeee')
     }
 });
 
@@ -88,6 +88,7 @@ btnConfirm.addEventListener('click', e => {
     
     let inputs = document.getElementsByTagName('input');
     let warning;
+    let allInputsHaveValues = true;
 
     let warnings = document.querySelectorAll('.error');
     for (let i = 0; i < warnings.length; i++) {
@@ -106,5 +107,19 @@ btnConfirm.addEventListener('click', e => {
                 warning.remove();
             }, 3000);
         }
+    };
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value === "") {
+          allInputsHaveValues = false;
+          break;
+        }
     }
+      
+    if (allInputsHaveValues) {
+        form.classList.add('hidden');
+        result.classList.remove('hidden');
+    }
+
+
 });
